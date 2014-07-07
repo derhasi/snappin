967acc6a599c8dbb84976075034de2b49dac69ac
Johannes Haseitl <johannes@undpaul.de>
2014-07-07 21:21:18 +0200

    Implementing basic area selection: temporary disabled screens hotting

--- src/background.js
+++ src/background.js
@@ -3,9 +3,11 @@
  *
  */
 chrome.browserAction.onClicked.addListener(function(tab) {
-  // Load the content script after jquery has been loaded.
+  // Load the content script after jquery has been loaded and css was added.
   chrome.tabs.executeScript(null, { file: "bower_components/jquery/dist/jquery.js" }, function() {
-    chrome.tabs.executeScript(null, { file: "src/content.js" });
+    chrome.tabs.insertCSS(null, {file: "css/content.css"}, function() {
+      chrome.tabs.executeScript(null, { file: "src/content.js" });
+    });
   });
 
   chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
--- src/content.js
+++ src/content.js
@@ -1,18 +1,54 @@
 
 (function ($) {
 
-  $('body').css('background', 'red');
+  // Remove any olverlays on init.
+  $('#snappinOverlay, #snappinClose, #snappinArea').remove();
 
-  console.log(top);
+  var overlay = $("<div id=\"snappinOverlay\"><div id=\"snappinClose\"></div></div>" ).appendTo("body");
 
-  $('body').click(function() {
-    var dimensions = {"x": 100, "y": 100, "w": 500, "h": 500};
 
+  var dimensions = {
+    top: 0,
+    left: 0,
+    width: 0,
+    height: 0
+  };
+
+  $('#snappinOverlay').mousedown(function (e) {
+
+    console.log(e);
+
+    $('#snappinArea').remove();
+    var area = $("<div id=\"snappinArea\"></div>" ).appendTo("#snappinOverlay");
+
+    dimensions.left = e.pageX;
+    dimensions.top = e.pageY;
+
+    console.log(dimensions);
+
+    // Set the position of the area.
+    area.offset(dimensions);
+
+    // Update area dimensions on mouse move.
+    $('#snappinOverlay').mousemove(function(e2) {
+      area.width(e2.pageX - dimensions.left);
+      area.height(e2.pageY - dimensions.top);
+    });
+
+    // On mouse up, we end the area update.
+    $('#snappinOverlay').mouseup(function() {
+      $('#snappinOverlay').unbind('mousemove');
+    })
+
+    // @todo: initScreenshot()
+
+  });
+
+  function initScreenshot(dimensions) {
     chrome.runtime.sendMessage({dimensions: dimensions}, function(response) {
       console.log(response.farewell);
     });
-
-  });
+  }
 
 
 })(jQuery);
