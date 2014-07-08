2e9e95163220cf081d2510deec1bda7c5748bf45
Johannes Haseitl <johannes@undpaul.de>
2014-07-08 14:46:09 +0200

    Take screenshot from drawed rectangle.

--- src/content.js
+++ src/content.js
@@ -2,52 +2,75 @@
 (function ($) {
 
   // Remove any olverlays on init.
-  $('#snappinOverlay, #snappinClose, #snappinArea').remove();
+  removeOverlay();
 
   var overlay = $("<div id=\"snappinOverlay\"><div id=\"snappinClose\"></div></div>" ).appendTo("body");
 
-
-  var dimensions = {
-    top: 0,
-    left: 0,
-    width: 0,
-    height: 0
-  };
-
+  /**
+   * Place area on mousedown.
+   */
   overlay.mousedown(function (e) {
-
-    console.log(e);
 
     $('#snappinArea').remove();
     var area = $("<div id=\"snappinArea\"></div>" ).appendTo(overlay);
 
-    dimensions.left = e.pageX;
-    dimensions.top = e.pageY;
-
-    console.log(dimensions);
+    var pos = {
+      left: e.pageX,
+      top: e.pageY
+    }
 
     // Set the position of the area.
-    area.offset(dimensions);
+    area.offset(pos);
 
     // Update area dimensions on mouse move.
     overlay.mousemove(function(e2) {
-      area.width(e2.pageX - dimensions.left);
-      area.height(e2.pageY - dimensions.top);
+      area.width(e2.pageX - pos.left);
+      area.height(e2.pageY - pos.top);
     });
 
     // On mouse up, we end the area update.
     overlay.mouseup(function() {
       overlay.unbind('mousemove');
+
+      takeScreenshot(overlay, area);
     })
 
-    // @todo: initScreenshot()
 
   });
 
-  function initScreenshot(dimensions) {
-    chrome.runtime.sendMessage({dimensions: dimensions}, function(response) {
-      console.log(response.farewell);
+  /**
+   * Helper to remove overlay.
+   */
+  function removeOverlay() {
+    $('#snappinOverlay, #snappinClose, #snappinArea').remove();
+  }
+
+  /**
+   * Helper to initialise the screenshot for the given overlay and selected
+   * area.
+   */
+  function takeScreenshot(overlay, area) {
+
+    var overlayOffset = overlay.offset();
+    var areaOffset = area.offset();
+
+    var dim = {
+      x: areaOffset.left - overlayOffset.left,
+      y: areaOffset.top - overlayOffset.top,
+      w: area.outerWidth(),
+      h: area.outerHeight()
+    };
+    console.log('dim', dim);
+
+    // Remove the overlay, before we take the screenshot.
+    removeOverlay();
+
+    // Send message to the backgrund page, so a screenshot can be taken.
+    chrome.runtime.sendMessage({dimensions: dim}, function(response) {
+      console.log(response);
     });
+
+
   }
 
 
